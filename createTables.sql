2d4d7655b35c9c8dbbe55cb2275e3c1d2a350c64
Ran He <he.ra@northeastern.edu>
2019-04-08 18:31:49 -0400

    CUSTOMER TABLE

--- createTables.sql
+++ createTables.sql
@@ -1,1 +1,21 @@
+CREATE DATABASE db_final;
 
+USE db_final;
+
+CREATE TABLE IF NOT EXISTS `db_final`.`customer` (
+  `customer_id` VARCHAR(100) NOT NULL,
+  `customer_name` VARCHAR(45) NOT NULL,
+  `customer_phone` CHAR(10) NOT NULL,
+  `customer_email` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`customer_id`));
+  
+INSERT  INTO `db_final`.`customer` (
+`customer_id`, 
+`customer_name`, 
+`customer_phone`, 
+`customer_email`) 
+VALUES (
+'8ee7b542-c493-404e-bdd4-19f2a4267708',
+ 'Ankit Acciaio',
+ '2615102674',
+ 'Ankit@gmail.com');
