9df79f9a26ba2036940fa3d2f2878999fb819909
Ran He <he.ra@northeastern.edu>
2019-04-08 18:46:35 -0400

    employee table

--- createTables.sql
+++ createTables.sql
@@ -14,6 +14,20 @@ CREATE TABLE IF NOT EXISTS `db_final`.`timesheet` (
   `year` INT NOT NULL,
   `month` INT NOT NULL,
   PRIMARY KEY (`timesheet_id`));
+               
+CREATE TABLE `db_final`.`employee` (
+  `employee_id` VARCHAR(100) NOT NULL,
+  `employee_name` VARCHAR(45) NOT NULL,
+  `employee_phone` CHAR(10) NOT NULL,
+  `salary` DECIMAL(10,1) NOT NULL,
+  `timesheet_id` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`employee_id`),
+  INDEX `timesheet_id_idx` (`timesheet_id` ASC) VISIBLE,
+  CONSTRAINT `timesheetID`
+    FOREIGN KEY (`timesheet_id`)
+    REFERENCES `db_final`.`timesheet` (`timesheet_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
   
 INSERT  INTO `db_final`.`customer` (
 `customer_id`, 
