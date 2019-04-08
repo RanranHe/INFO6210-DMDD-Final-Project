4c852f2127fb80f181b295e5211f27f3ae8a77da
Ran He <he.ra@northeastern.edu>
2019-04-08 18:37:57 -0400

    timesheet table

--- createTables.sql
+++ createTables.sql
@@ -8,6 +8,12 @@ CREATE TABLE IF NOT EXISTS `db_final`.`customer` (
   `customer_phone` CHAR(10) NOT NULL,
   `customer_email` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`customer_id`));
+              
+CREATE TABLE IF NOT EXISTS `db_final`.`timesheet` (
+  `timesheet_id` VARCHAR(100) NOT NULL,
+  `year` INT NOT NULL,
+  `month` INT NOT NULL,
+  PRIMARY KEY (`timesheet_id`));
   
 INSERT  INTO `db_final`.`customer` (
 `customer_id`, 
