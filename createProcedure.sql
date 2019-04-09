f787b87f862f840fbff25170ffc3774877327cdb
Ran He <he.ra@northeastern.edu>
2019-04-09 00:24:23 -0400

    Update createProcedure.sql

--- createProcedure.sql
+++ createProcedure.sql
@@ -7,9 +7,9 @@ DELIMITER $$
 USE `db_final`$$
 CREATE PROCEDURE `update_stock` (IN id VARCHAR(100), IN new_stock INT)
 BEGIN
-		 UPDATE `db_final`.`book` 
-         SET stock=new_stock
-         WHERE book_id=id;
+    UPDATE `db_final`.`book` 
+    SET stock=new_stock
+    WHERE book_id=id;
 END$$
 
 DELIMITER ;
