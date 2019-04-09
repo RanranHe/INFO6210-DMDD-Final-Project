bec36b88b9b80121a1bb2a5ca6b206f4d75d3ec1
Ran He <he.ra@northeastern.edu>
2019-04-08 20:38:37 -0400

    Create insertData.sql

---
+++ insertData.sql
@@ -0,0 +1,5 @@
+INSERT INTO `db_final`.`author` (`author_id`, `author_name`, `date_of_birth`) 
+VALUES ('2a977f4b-abb6-4cf3-a578-67f8e840ae60', 'Sakshi Zunino', '1967-12-12');
+
+INSERT INTO `db_final`.`author` (`author_id`, `author_name`, `date_of_birth`) 
+VALUES ('0118be2d-e9ef-4e87-9a74-b3b894cb9cfa', 'Kip Both', '1968-12-12');
