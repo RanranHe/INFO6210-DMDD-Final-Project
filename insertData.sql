c8567e694d82dd00f300063390043cd9df8f0b5c
Ran He <he.ra@northeastern.edu>
2019-04-09 00:04:40 -0400

    Update insertData.sql

--- insertData.sql
+++ insertData.sql
@@ -35,6 +35,16 @@ VALUES ('ff2e7858-8360-430f-a26a-16538fd5af6d', 'Harvard Common Press', 'US', 'B
 INSERT INTO `db_final`.`publisher` (`publisher_id`, `publisher_name`, `country`, `city`) 
 VALUES ('dd4e9d89-cf5a-4564-8b0f-2a0489c5fa5e', 'National Braille Press', 'US', 'Boston');
 
+/* -------------------------------------------Location Table--------------------------------------------------*/
+INSERT INTO `db_final`.`location` (`location_id`, `shelf_no`, `row`, `column`) 
+VALUES ('e487f555-dfdf-4c7f-b4e8-a59fb6524fc5', '1', '1', '1');
+
+INSERT INTO `db_final`.`location` (`location_id`, `shelf_no`, `row`, `column`) 
+VALUES ('14221076-619e-4bc9-b2f7-3eb060541bf4', '2', '4', '3');
+
+INSERT INTO `db_final`.`location` (`location_id`, `shelf_no`, `row`, `column`) 
+VALUES ('ffb81aee-7d4c-49ec-a6c5-74791e685043 ', '2', '1', '1');
+
 /* -------------------------------------------Timesheet Table--------------------------------------------------*/
 INSERT INTO `db_final`.`timesheet` (`timesheet_id`, `year`, `month`) 
 VALUES ('99a60782-b681-4e84-b8a8-2b0a98c8fa6f', '2019', '1');
@@ -57,3 +67,19 @@ VALUES ('76f6b20e-9c24-4468-9c92-32c49dd84373', 'Nevan Ríos', '5174750581', '60
 INSERT INTO `db_final`.`employee` (`employee_id`, `employee_name`, `employee_phone`, `salary`, `timesheet_id`) 
 VALUES ('23867168-4808-4a69-a944-a7024d671794', 'Antonia Faure', 
 '6626957215', '4000', '38ed6367-7377-4b12-a1b0-7dd9893cb00d');
+
+/* -------------------------------------------Book Table--------------------------------------------------*/
+INSERT INTO `db_final`.`book` (`book_id`, `price`, `location_id`, `stock`, `book_name`, 
+`publisher_id`, `author_id`, `publisher_date`, `description`, `category_id`) 
+VALUES ('cf4e3491-cfcc-4dd0-bef1-1e7ff602e5c7', '8', '14221076-619e-4bc9-b2f7-3eb060541bf4', '100', 
+'121 Good-Eating Tips', 'dd4e9d89-cf5a-4564-8b0f-2a0489c5fa5e', '0118be2d-e9ef-4e87-9a74-b3b894cb9cfa', 
+'2010-10-23', 'This is your guide to eating healthfully the quick-and-easy way!',
+'2616c440-509d-43c3-ad03-78571c41a695');
+
+INSERT INTO `db_final`.`book` (`book_id`, `price`, `location_id`, `stock`, `book_name`, 
+`publisher_id`, `author_id`, `publisher_date`, `description`, `category_id`) 
+VALUES ('63534709-a711-44a9-8baa-6574a2e4304e', '8', 'e487f555-dfdf-4c7f-b4e8-a59fb6524fc5', '100', 
+'12 Amazing Tricks', 'dd4e9d89-cf5a-4564-8b0f-2a0489c5fa5e', '2a977f4b-abb6-4cf3-a578-67f8e840ae60', 
+'2016-11-10', 'Kids will love amazing their friends with these twelve magic tricks that use items found around the house.',
+'8522ad28-9718-47e1-8387-9597e2411e7b');
+
