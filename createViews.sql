a3a38c240ba2e2344b656a2549d9e54cd603eb91
Ran He <he.ra@northeastern.edu>
2019-04-09 17:13:45 -0400

    Update createViews.sql

--- createViews.sql
+++ createViews.sql
@@ -11,4 +11,5 @@ SELECT B.book_name, P.publisher_name, A.author_name, C.category_name, B.price, L
 FROM timesheet AS T INNER JOIN period AS P ON T.timesheet_id
 INNER JOIN publisher AS P ON B.publisher_id=P.publisher_id
 INNER JOIN category AS C ON B.category_id=C.category_id
-INNER JOIN author AS A ON B.author_id=A.author_id;
+INNER JOIN author AS A ON B.author_id=A.author_id
+ORDER BY T.year, T.month, P.start_time DESC;
