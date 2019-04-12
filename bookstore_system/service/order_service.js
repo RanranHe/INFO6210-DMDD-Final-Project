88283bf7968d3ee9170757fbdc96fc009470e782
Ranran He <cangxuexiao@gmail.com>
2019-04-11 20:51:53 -0400

    finish order back end

--- bookstore_system/service/order_service.js
+++ bookstore_system/service/order_service.js
@@ -58,11 +58,90 @@ function getOrderDetails(orderId) {
         let sql = "SELECT * FROM order_details_view WHERE order_id='" + orderId+"';";
         con.query(sql, function (err, result) {
             if (err) {
-                console.log("[VIEW ORDERS ERROR]: " + err);
+                console.log("[VIEW ORDER DETAILS ERROR]: " + err);
                 throw err;
             } else {
-                console.log(result);
                 resolve(result);
+            }
+        });
+    });
+}
+
+function createOrder(order_id, customer_id, order_date, employee_id) {
+    return new Promise(function (resolve) {
+        let sql = "INSERT INTO `db_final`.`order` (`order_id`, `customer_id`, `order_date`, `employee_id`) " +
+            "VALUES ('" + order_id + "', '" + customer_id + "', '" + order_date + "', '" + employee_id + "')";
+        con.query(sql, function (err) {
+            if (err) {
+                console.log("[INSERT ORDER ERROR]: " + err);
+                resolve(false);
+                throw err;
+            } else {
+                resolve(true);
+            }
+        });
+    });
+}
+
+function createItem(item_id, order_id, quantity, book_id) {
+    return new Promise(function (resolve) {
+        let sql = "CALL add_item('" + item_id + "', '" + order_id + "', '" + quantity + "', '" +
+            book_id + "', @a, @b, @c, @d);";
+        con.query(sql, function (err, result) {
+            if (err) {
+                console.log("[INSERT ITEM ERROR]: " + err);
+                throw err;
+            } else {
+                console.log(result[0][0]);
+                resolve(result[0][0]);
+            }
+        });
+    });
+}
+
+function checkItemId(item_id) {
+    return new Promise(function (resolve) {
+        let sql = "SELECT * FROM db_final.`item` WHERE item_id='" + item_id + "'";
+        con.query(sql, function (err, result) {
+            if (err) {
+                console.log("[CHECK ITEM ID ERROR]: " + err);
+                throw err;
+            }
+            if (result[0] == null || result[0]===undefined) {
+                console.log(`[SEARCH FAILED]: NO SUCH ITEM`);
+                resolve(false);
+            } else {
+                resolve(true);
+            }
+        });
+    });
+}
+
+function deleteItem(item_id) {
+    return new Promise(function (resolve) {
+        let sql = "DELETE FROM `db_final`.`item` WHERE `item_id`='" + item_id + "'";
+        con.query(sql, function (err) {
+            if (err) {
+                console.log("[DELETE ITEM ERROR]: " + err);
+                resolve(false);
+                throw err;
+            } else {
+                resolve(true);
+            }
+        });
+    });
+}
+
+function deleteOrder(order_id) {
+    return new Promise(function (resolve) {
+        let sql = "CALL delete_order('" + order_id + "')";
+        con.query(sql, function (err) {
+            if (err) {
+                console.log("[DELETE ORDER ERROR]: " + err);
+                resolve(false);
+                throw err;
+            } else {
+                resolve(true);
             }
         });
     });
@@ -72,6 +151,11 @@ module.exports = {
     getTotalPrice,
     checkOrderId,
     getAllOrders,
-    getOrderDetails
+    getOrderDetails,
+    createOrder,
+    createItem,
+    checkItemId,
+    deleteItem,
+    deleteOrder
 };
 
