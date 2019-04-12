1f6e416531baa5522a056387a79c8cfe94678ca6
Ran He <cangxuexiao@gmail.com>
2019-04-12 04:01:50 -0400

    Update createViews.sql

--- createViews.sql
+++ createViews.sql
@@ -4,14 +4,13 @@ SELECT B.book_id, B.book_name, publisher_name, author_name, C.category_name, B.p
 FROM book AS B INNER JOIN location AS L ON B.location_id=L.location_id
 INNER JOIN publisher AS P ON B.publisher_id=P.publisher_id
 INNER JOIN category AS C ON B.category_id=C.category_id
-INNER JOIN author AS A ON B.author_id=A.author_id;
+INNER JOIN author AS A ON B.author_id=A.author_id
+ORDER BY B.book_name ASC;
 
 CREATE OR REPLACE VIEW `timesheet_details` AS
-SELECT B.book_name, P.publisher_name, A.author_name, C.category_name, B.price, L.shelf_no, L.row, L.column, B.stock
-FROM timesheet AS T INNER JOIN period AS P ON T.timesheet_id
-INNER JOIN publisher AS P ON B.publisher_id=P.publisher_id
-INNER JOIN category AS C ON B.category_id=C.category_id
-INNER JOIN author AS A ON B.author_id=A.author_id
+SELECT T.timesheet_id, E.employee_name, T.year, T.month, P.start_time, P.end_time
+FROM timesheet AS T INNER JOIN period AS P ON T.timesheet_id=P.timesheet_id
+INNER JOIN employee AS E ON T.employee_id=E.employee_id
 ORDER BY T.year, T.month, P.start_time DESC;
 
 CREATE OR REPLACE VIEW `order_view` AS
