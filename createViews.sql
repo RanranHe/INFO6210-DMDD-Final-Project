01088e40366fe804b879138052e72726259e4ef1
Ran He <he.ra@northeastern.edu>
2019-04-11 20:49:49 -0400

    Update createViews.sql

--- createViews.sql
+++ createViews.sql
@@ -14,11 +14,11 @@ INNER JOIN category AS C ON B.category_id=C.category_id
 INNER JOIN author AS A ON B.author_id=A.author_id
 ORDER BY T.year, T.month, P.start_time DESC;
 
-CREATE OR REPLAC VIEW `order_view` AS
+CREATE OR REPLACE VIEW `order_view` AS
 SELECT O.order_id, C.customer_name, O.order_date, E.employee_name
 FROM `order` AS O INNER JOIN `employee` AS E ON O.employee_id=E.employee_id
 INNER JOIN `customer` AS C ON O.customer_id=C.customer_id;
 
-CREATE  OR REPLACE VIEW `order_details_view` AS
-SELECT I.order_id, I.quantity, B.book_name, B.price
+CREATE OR REPLACE VIEW `order_details_view` AS
+SELECT I.order_id, I.quantity, B.book_id, B.book_name, B.price
 FROM `item` AS I INNER JOIN `book` AS B ON I.book_id=B.book_id;
